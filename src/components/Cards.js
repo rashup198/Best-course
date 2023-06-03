cdb884a1cdbc51afd7bf94823ad4cc922b372564
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-03 22:26:59 +0530

    updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card';
 
-const Cards = () => {
+const Cards = ({courses}) => {
+
+    let allCourses=[];
+    //returns you a list of all courses recived from the api response
+    const getCourses=()=>{
+        console.log(courses);
+        Object.values(courses).forEach((courseCategory)=>{
+            courseCategory.forEach((course)=>{
+                allCourses.push(course);
+            })
+        })
+        return allCourses;
+    }
+
+
   return (
     <div>
-      
+      {getCourses().map((course)=>{
+        return(<Card key = {course.id} course={course}></Card>)
+      })
+    }
     </div>
   )
 }
