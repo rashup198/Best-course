805860bad42664b70590f7768f7b469762bb67fd
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-03 21:04:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from "react";
 import {apiUrl, filterData} from "./data";
+import Navbar from './components/Navbar'
+import Filter from "./components/Filter";
+import Cards from './components/Cards'
+import { toast } from "react-toastify";
+
 function App () {
- const []
+
+  const [curses, setCourses] = useState()
+  useEffect(()=>{
+    const fetchData=  async()=>{
+      try {
+        const res= await fetch(apiUrl);
+        const data = await res.json();
+
+        //save data into a variable
+        console.log(data);
+      } catch (error) {
+        toast.error("something went wrong")
+      }
+    }
+    fetchData();
+  },[])
 
 
   return(
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/reactToastify.css"
+// import "react-toastify/dist/reactToastify.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
