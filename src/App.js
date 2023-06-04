456b6e1d164f76a32ddc265a8fe9c65c829430bc
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-04 11:23:16 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -4,37 +4,54 @@ import Navbar from './components/Navbar'
 import Filter from "./components/Filter";
 import Cards from './components/Cards'
 import { toast } from "react-toastify";
-
+import Spinner from "./components/Spinner";
 function App () {
 
-  const [courses, setCourses] = useState(null)
-  useEffect(()=>{
-    const fetchData=  async()=>{
-      try {
-        const res= await fetch(apiUrl);
-        const output = await res.json();
+  const[courses,setCourses]=useState(null);
+  const[loading, setLoading]= useState(true);
+
+
+ async function fetchData(){
+  setLoading(true);
+        try {
+        const response= await fetch(apiUrl);
+        const output = await response.json();
 
         //save data into a variable
-        setCourses(output.data);
+        setCourses(output);
         console.log("courses value");
         console.log(courses);
       } catch (error) {
         toast.error("something went wrong")
       }
+      setLoading(false);
     }
+    
+
+
+  useEffect(()=>{
     fetchData();
-  },[])
+  },[]);
 
 
   return(
     <div>
+    <div>
       <Navbar></Navbar>
-
+    </div>
+    <div>
       <Filter filterData={filterData}></Filter>
+    </div>
 
-      <Cards courses={courses}></Cards>
+    <div>
+      {
+        loading?(<Spinner></Spinner>) : (<Cards courses={courses}></Cards>)
+      }
+      
+    </div>    
+
     </div>
   );
-};
+}
 
 export default App;
