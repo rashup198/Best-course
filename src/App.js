4115e9325243eedf9df72d4cb7236347d60aee9b
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-04 16:29:52 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {apiUrl, filterData} from "./data";
 import Navbar from './components/Navbar'
 import Filter from "./components/Filter";
 import Cards from './components/Cards'
-import { toast } from "react-toastify";
+import { ToastContainer,toast } from "react-toastify";
 import Spinner from "./components/Spinner";
+import 'react-toastify/dist/ReactToastify.css';
 function App () {
 
   const[courses,setCourses]=useState([]);
   const[loading, setLoading]= useState(true);
+  const[category, setCategory] =useState(filterData[0].title);
 
 
  async function fetchData(){
@@ -42,11 +44,11 @@ function App () {
 
     <div className="bg-bgDark2">
     <div>
-      <Filter filterData={filterData}></Filter>
+      <Filter filterData={filterData} category={category} setCategory={setCategory}></Filter>
     </div>
     <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
       {
-        loading?(<Spinner></Spinner>) : (<Cards courses={courses}></Cards>)
+        loading?(<Spinner></Spinner>) : (<Cards courses={courses} category={category}></Cards>)
       }
       
     </div>    
