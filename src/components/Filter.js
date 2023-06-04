807696e381185550b22125ca26085b82bdb08db5
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-04 16:30:20 +0530

    updated filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 const Filter = (props) => {
   let filterData = props.filterData;
+  let category = props.category;
+  let setCategory= props.setCategory;
+
+  function filterHandler(title){
+    setCategory(title)
+  }
+
+
+
   return (
-    <div>
+    <div className='w-11/12 felx flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center'>
       {filterData.map((data)=>(
-        <button key={data.id}>
-            {data.title};
+        <button key={data.id} className={`text-lg px-2 py-2 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300`}onClick={()=>filterHandler(data.title)}>
+            {data.title}
+           
         </button>
       ) )}
     </div>
