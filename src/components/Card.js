424af408e1d79dd084a91b29e57d14530d2371c6
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-04 12:30:02 +0530

    updated card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,18 +4,19 @@ import { FcLike } from "react-icons/fc";
 const Card = (props) => {
   let course= props.course;
   return (
-   <div>
-        <div>
+   <div className='w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden'>
+        <div className='relative '>
           <img src={course.image.url}></img>
+            <div>
+              <button className='rounded-full w-[40px] h-[40px] bg-white absolute right-2 bottom-2 grid place-items-center'>
+                <FcLike fontSize='1.75rem'></FcLike>
+              </button>
+          </div>
         </div>
-        <div>
-          <button>
-            <FcLike></FcLike>
-          </button>
-        </div>
-        <div>
-          <p>{course.title}</p>
-          <p>{course.description}</p>
+        
+        <div className='p-4'>
+          <p className='text-white font-semibold text-lg leading-6'>{course.title}</p>
+          <p className='mt-2 text-white'>{course.description}</p>
         </div>
    </div>
   )
