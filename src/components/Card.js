9c937de18887299e66e22b7c28fdcf3a5aedda94
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-04 16:30:09 +0530

    updated cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,56 @@
 import React from 'react'
-import { FcLike } from "react-icons/fc";
+import { FcLike,FcLikePlaceholder} from "react-icons/fc";
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Card = (props) => {
   let course= props.course;
+  let likedCourses =props.likedCourses;
+  let setLikeCourses =props.setLikeCourses;
+  function clickHandler(){
+    if(likedCourses.includes(course.id)){
+      //pehle se liked hai
+      setLikeCourses((prev)=>{
+        prev.filter((cid)=>(cid!==course.id))
+        
+      })
+      toast.warning("like removed");
+    }
+    else{
+      //pehle se liked nhi hai course
+      //then  insert karo ye course liked course me
+
+      if(likedCourses.length===0){
+        setLikeCourses([course.id]);
+      }
+      else{
+        setLikeCourses((prev)=>[...prev, course.id])
+      }
+      toast.success("Liked Successfully")
+
+
+    }
+  }
   return (
    <div className='w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden'>
         <div className='relative '>
           <img src={course.image.url}></img>
-            <div>
-              <button className='rounded-full w-[40px] h-[40px] bg-white absolute right-2 bottom-2 grid place-items-center'>
-                <FcLike fontSize='1.75rem'></FcLike>
+            <div className='rounded-full w-[40px] h-[40px] bg-white absolute right-2 -bottom-4 grid place-items-center'>
+              <button onClick={clickHandler}>
+                {!likedCourses.includes(course.id)?(<FcLikePlaceholder fontSize="1.75rem"></FcLikePlaceholder>):(<FcLike fontSize="1.75rem"></FcLike>)
+                  }
               </button>
           </div>
         </div>
         
         <div className='p-4'>
           <p className='text-white font-semibold text-lg leading-6'>{course.title}</p>
-          <p className='mt-2 text-white'>{course.description}</p>
+          <p className='mt-2 text-white'>
+            {
+              course.description.length>100 ? `${(course.description.substr(0,100))}.....`:(course.description)
+            }
+          </p>
         </div>
    </div>
   )
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
+
 
 const Cards = (props) => {
 
     let courses=props.courses;
+    let category=props.category;
+
+    const [likedCourses, setLikeCourses] = useState([]);
     //returns you a list of all courses recived from the api response
 
     function getCourses(){
+      if(category==="All"){
        let allCourses=[];
         Object.values(courses).forEach(array=>{
             array.forEach(courseData=>{
@@ -14,13 +19,19 @@ const Cards = (props) => {
             })
         })
         return allCourses;
+      }
+
+      else{
+        //isme sirf specific categiry ka data enter hoga
+        return courses[category]
+      }
     }
 
 
   return (
     <div className='flex flex-wrap justify-center gap-4 mb-4' >
       {getCourses().map((course)=>{
-        return(<Card key = {course.id} course={course}></Card>)
+        return(<Card key = {course.id} course={course} likedCourses={likedCourses} setLikeCourses={setLikeCourses}></Card>)
       })
     }
     </div>
