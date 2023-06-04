f062f603403f096147b9fedeec6eeadde3ec5b50
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-04 16:30:30 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar=()=>{
     return(
-        <div>
-            <nav>
-                <h1>Top Courses</h1>
+        <div >
+            <nav className="bg-blue-900 py-4 ">
+                <h1 className="text-3xl font-bold text-center text-white">Top Courses</h1>
             </nav>
         </div>
     )
